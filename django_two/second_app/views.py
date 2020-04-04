dc085da1580afe08ee837f6ce7c221a613dad6ed
YashMunjal <yashmu45@gmail.com>
2020-04-04 14:59:24 +0530

    jj

--- django_two/second_app/views.py
+++ django_two/second_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    my_dict = {'insert_content':"Hello i am from second app"}
+    return render(request,'second_app/index.html',context=my_dict)
